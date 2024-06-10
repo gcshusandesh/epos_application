9d66fa193bf39c19d8e7317d3657e4219f4641e9
Shusandesh GC <gcshusandesh@gmail.com>
2024-06-10 20:53:59 +0100

    making menu

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,5 @@
+import 'package:epos_application/components/data.dart';
+import 'package:epos_application/components/size_config.dart';
 import 'package:epos_application/providers/info_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -13,27 +15,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   bool init = true;
+  late double height;
+  late double width;
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
     if (init) {
+      //initialize size config at the very beginning
+      SizeConfig().init(context);
+      height = SizeConfig.safeBlockVertical;
+      width = SizeConfig.safeBlockHorizontal;
       Provider.of<InfoProvider>(context, listen: false).getTestData();
-
       init = false;
     }
   }
@@ -41,56 +37,70 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SizedBox(
+              height: height * 2,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            buildTitleText("Dashboard Hub" , Data.primaryTextColor, width),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  height: height * 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    buildMenuItem(Icons.ac_unit, "Ac Unit", Colors.red),
+                    buildMenuItem(Icons.ac_unit, "Ac Unit", Colors.red),
+                    buildMenuItem(Icons.ac_unit, "Ac Unit", Colors.red),
+                  ],
+                ),
+                SizedBox(
+                  height: height * 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    buildMenuItem(Icons.ac_unit, "Ac Unit", Colors.red),
+                    buildMenuItem(Icons.ac_unit, "Ac Unit", Colors.red),
+                    buildMenuItem(Icons.ac_unit, "Ac Unit", Colors.red),
+                  ],
+                ),
+              ],
             ),
-          ],
+
+          ]
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  InkWell buildMenuItem(IconData icon, String text, Color color) {
+    return InkWell(
+      onTap: () {
+        print("Tapped $text");
+      },
+      child: Expanded(
+        child: Container(
+                color: Colors.teal,
+                child: Column(
+                  children: [
+                    Icon(icon, color: color, size: width * 10),
+                    buildBodyText(
+                      text,
+                      color,
+                      width,
+                    ),
+                  ],
+                ),
+              ),
+      ),
     );
   }
 }
