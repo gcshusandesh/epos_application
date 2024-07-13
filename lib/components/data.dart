2af27bae3c7020a3099d0b8b8487dd35e39ce2a5
Shusandesh GC <gcshusandesh@gmail.com>
2024-07-13 19:38:53 +0545

    network image loader

--- lib/components/data.dart
+++ lib/components/data.dart
@@ -121,6 +121,18 @@ Widget buildImage(
         ? Image.network(
             path,
             fit: fit,
+            loadingBuilder: (BuildContext context, Widget child,
+                ImageChunkEvent? loadingProgress) {
+              if (loadingProgress == null) return child;
+              return Center(
+                child: CircularProgressIndicator(
+                  value: loadingProgress.expectedTotalBytes != null
+                      ? loadingProgress.cumulativeBytesLoaded /
+                          loadingProgress.expectedTotalBytes!
+                      : null,
+                ),
+              );
+            },
           )
         : Image.asset(
             path,
